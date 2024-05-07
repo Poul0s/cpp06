19c31699fe41d002c8e08348f71c759d23084e68
psalame <psalame@student.42angouleme.fr>
2024-05-07 08:39:04 +0200

    changed convertion type from istringstream to strtol, strtold, strtof for overflow issue with double

--- ex00/ScalarConvertor.cpp
+++ ex00/ScalarConvertor.cpp
@@ -6,17 +6,17 @@
 /*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 22:27:52 by psalame           #+#    #+#             */
-/*   Updated: 2024/05/06 14:44:03 by psalame          ###   ########.fr       */
+/*   Updated: 2024/05/07 08:37:53 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConvertor.hpp"
 #include <iostream>
-#include <sstream>
 #include <limits>
-#include <string>
 #include <cmath>
 #include <iomanip>
+#include <cstdlib>
+#include <cerrno>
 
 ScalarConvertor::ScalarConvertor()
 {
@@ -49,31 +49,39 @@ static bool	isFloat(std::string &str)
 		str = str.substr(0, str.size() - 1);
 	}
 
-	std::istringstream	iss(str);
-	float				nb;
-
-	iss >> nb;
-	return (iss.eof() && !iss.fail() && !iss.bad());
+	char	*endptr;
+	float	nb = strtof(str.c_str(), &endptr);
+	if (*endptr != '\0')
+	{
+		return (false);
+	}
+	if (nb == std::numeric_limits<float>::infinity() || nb == -std::numeric_limits<float>::infinity() || std::isnan(nb))
+		return (false);
+	return (true);
 }
 
 static bool	isInt(std::string &str)
 {
-	std::istringstream	iss(str);
-	int 				nb;
-
-	iss >> nb;
-	return (iss.eof() && !iss.fail()); 
+	char	*endptr;
+	errno = 0;
+	long int	nb = strtol(str.c_str(), &endptr, 10);
+	if (*endptr != '\0' || errno == ERANGE || nb < std::numeric_limits<int>::min() || nb > std::numeric_limits<int>::max())
+		return (false);
+	return (true);
 }
 
 static bool	isDouble(std::string &str)
 {
 	if (str == "inf" || str == "+inf" || str == "-inf" || str == "nan")
 		return (true);
-	std::istringstream	iss(str);
-	double 				nb;
 
-	iss >> nb;
-	return (iss.eof() && !iss.fail() && !iss.bad()); 
+	char	*endptr;
+	double	nb = strtod(str.c_str(), &endptr);
+	if (*endptr != '\0')
+		return (false);
+	if (nb == std::numeric_limits<double>::infinity() || nb == -std::numeric_limits<double>::infinity() || std::isnan(nb))
+		return (false);
+	return (true);
 }
 
 static short	getStringType(std::string &str)
@@ -82,12 +90,12 @@ static short	getStringType(std::string &str)
 		return (-1);
 	else if (str.size() == 1 && !isdigit(str[0]))
 		return (0);
-	else if (isDouble(str))
-		return (3);
 	else if (str[str.size() - 1] == 'f' && isFloat(str))
 		return (2);
 	else if (isInt(str))
 		return (1);
+	else if (isDouble(str))
+		return (3);
 	return (-1);
 }
 
@@ -103,9 +111,7 @@ static void	convertFromChar(std::string &str, std::ostream &os)
 
 static void	convertFromInt(std::string &str, std::ostream &os)
 {
-	int					nb;
-	std::istringstream	iss(str);
-	iss >> nb;
+	int	nb = static_cast<int>(strtol(str.c_str(), NULL, 10));
 
 	if (nb < std::numeric_limits<char>::min() || nb > std::numeric_limits<char>::max())
 		os << "char: impossible" << std::endl;
@@ -129,8 +135,7 @@ static void	convertFromFloat(std::string &str, std::ostream &os)
 		nb = std::numeric_limits<float>::quiet_NaN();
 	else
 	{
-		std::istringstream	iss(str);
-		iss >> nb;
+		nb = std::strtof(str.c_str(), NULL);
 	}
 
 
@@ -162,8 +167,7 @@ static void	convertFromDouble(std::string &str, std::ostream &os)
 		nb = std::numeric_limits<double>::quiet_NaN();
 	else
 	{
-		std::istringstream	iss(str);
-		iss >> nb;
+		nb = std::strtod(str.c_str(), NULL);
 	}
 
 	if (std::isnan(nb) || nb < std::numeric_limits<char>::min() || nb > std::numeric_limits<char>::max())
@@ -178,7 +182,7 @@ static void	convertFromDouble(std::string &str, std::ostream &os)
 	else
 		os << "int: " << static_cast<int>(nb) << std::endl;
 	
-	os << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(nb) << "f" << std::endl; // todo look setprecision
+	os << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(nb) << "f" << std::endl;
 
 	os << "double: " << std::fixed << std::setprecision(1) << nb << std::endl;
 }
