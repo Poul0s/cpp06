96464f1450bd43b29bdb535ae08338e660c46cae
psalame <psalame@student.42angouleme.fr>
2024-05-07 09:38:46 +0200

    fixed tester ex00 doesnt exit with error if KO or MKO

--- ex00/tests/tester.sh
+++ ex00/tests/tester.sh
@@ -23,12 +23,14 @@ while read -r line; do
 	valgrind --leak-check=full --track-fds=yes --error-exitcode=1 "$EXEC_NAME" "$line" > $INPUT_DIR/output_$test_nb.txt
 	if [ ! $? -eq 0 ]; then
 		echo "Test $test_nb: MKO"
+		exit 2
 	else
 		diff $INPUT_DIR/output_$test_nb.txt $INPUT_DIR/res$test_nb.txt
 		if [ $? -eq 0 ]; then
 			echo "Test $test_nb: OK"
 		else
 			echo "Test $test_nb: KO"
+			exit 1
 		fi
 	fi
 	((test_nb++))
