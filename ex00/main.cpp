45067c12e7722a3c912d9f2bd3884025e6b903b9
psalame <psalame@student.42angouleme.fr>
2024-05-07 09:31:26 +0200

    convert tester in bash script (forbidden functions in cpp tester)

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,29 +6,23 @@
 /*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 12:23:19 by psalame           #+#    #+#             */
-/*   Updated: 2024/05/06 13:40:00 by psalame          ###   ########.fr       */
+/*   Updated: 2024/05/07 09:26:02 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "ScalarConvertor.hpp"
 
-int	scalarTester(void);
-
 int	main(int ac, char **av)
 {
-	if (ac == 1)
-	{
-		return (scalarTester());
-	}
-	else if (ac == 2)
+	if (ac == 2)
 	{
 		std::string	str = av[1];
 		ScalarConvertor::convert(str);
 	}
-	if (ac > 2)
+	else
 	{
-		std::cout << "Usage: ./" << av[0] << " [str value]" << std::endl;
+		std::cout << "Usage: " << av[0] << " <value>" << std::endl;
 		return (1);
 	}
 }
---
+++ ex00/tests/tester.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+EXEC_NAME="../scalarConvert"
+INPUT_FILE="./input.txt"
+
+# if [ -v $1 ]; then
+# 	EXEC_NAME=$1
+# fi
+
+# if [ -v $2 ]; then
+# 	INPUT_FILE=$2
+# fi
+
+# echo "Executable: $EXEC_NAME"
+# echo "Input file: $INPUT_FILE"
+
+INPUT_DIR=$(dirname $INPUT_FILE)
+
+test_nb=1
+
+while read -r line; do
+	# echo "Input: $line"
+	valgrind --leak-check=full --track-fds=yes --error-exitcode=1 "$EXEC_NAME" "$line" > $INPUT_DIR/output_$test_nb.txt
+	if [ ! $? -eq 0 ]; then
+		echo "Test $test_nb: MKO"
+	else
+		diff $INPUT_DIR/output_$test_nb.txt $INPUT_DIR/res$test_nb.txt
+		if [ $? -eq 0 ]; then
+			echo "Test $test_nb: OK"
+		else
+			echo "Test $test_nb: KO"
+		fi
+	fi
+	((test_nb++))
+done < "$INPUT_FILE"
